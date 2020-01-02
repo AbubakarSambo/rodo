151631641e5846d8877d683f59203c55dce7eee5
abubakar sambo <abubakarsambo@abubakars-MacBook-Pro.local>
2020-01-02 16:57:38 +0100

    Updates

---
+++ precache-manifest.fb671f41a186159f4f136e550b83e8ac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b896c2fcc51c1e2afd5f0eba865dd6d2",
+    "url": "/rodo/index.html"
+  },
+  {
+    "revision": "2dce3386f0cfff3504b2",
+    "url": "/rodo/static/css/main.c115a361.chunk.css"
+  },
+  {
+    "revision": "2a33672cf5baa9f13257",
+    "url": "/rodo/static/js/2.3ad142bd.chunk.js"
+  },
+  {
+    "revision": "2dce3386f0cfff3504b2",
+    "url": "/rodo/static/js/main.a582d48a.chunk.js"
+  },
+  {
+    "revision": "3a0b38b070efbdc8f2a1",
+    "url": "/rodo/static/js/runtime-main.a6d79c89.js"
+  },
+  {
+    "revision": "f68422dce533b5b46744d1ec3e147e89",
+    "url": "/rodo/static/media/benefits.f68422dc.png"
+  },
+  {
+    "revision": "fd1930583e5285d078433e3c3a5bca1e",
+    "url": "/rodo/static/media/cloves.fd193058.png"
+  },
+  {
+    "revision": "95f3881f3ce1aee69e1ad68e5a1248e1",
+    "url": "/rodo/static/media/cold-cup.95f3881f.png"
+  },
+  {
+    "revision": "835e9bea976948626d395bff625d8e1d",
+    "url": "/rodo/static/media/ginger.835e9bea.png"
+  },
+  {
+    "revision": "e739ba0fa6cb588efefba159173f23b3",
+    "url": "/rodo/static/media/hibiscus.e739ba0f.png"
+  },
+  {
+    "revision": "6e0103e3d2e95e807058c3836c1beec4",
+    "url": "/rodo/static/media/hot-cup.6e0103e3.png"
+  },
+  {
+    "revision": "4490f52aa891a73d5a6fff01a7b43be4",
+    "url": "/rodo/static/media/mint.4490f52a.png"
+  },
+  {
+    "revision": "9f6a1ca8fe2b2b27bbcea671c4cf016e",
+    "url": "/rodo/static/media/pack.9f6a1ca8.png"
+  },
+  {
+    "revision": "98c807d709b9fbf372a88d75692bade3",
+    "url": "/rodo/static/media/rodo.98c807d7.png"
+  },
+  {
+    "revision": "b44c09f7faa5a698ce144d0208b822f0",
+    "url": "/rodo/static/media/thank-you-bg.b44c09f7.png"
+  }
+]);
